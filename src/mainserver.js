8417935614cb744fcab35982cefa900c48faf37d
KevinJN03 <114575604+KevinJN03@users.noreply.github.com>
2022-11-24 15:59:05 +0000

    added cors and change port

--- src/mainserver.js
+++ src/mainserver.js
@@ -1,13 +1,14 @@
 const db = require("./db/db")
 const express = require("express");
 const app = express();
+const cors = require("cors")
 const userRouter = require("./routes/user");
 const postRouter = require("./routes/post");
-
+app.use(cors())
 app.use(express.json());
 app.use("/user", userRouter)
 app.use("/post", postRouter)
-app.listen(3000, async () => {
+app.listen(5001, async () => {
     await db.sync()
     console.log("Server Listening")
 })
